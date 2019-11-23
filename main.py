93d667051d185b2b6184e84108674ff365dc72a4
Unwound <unwoundd@tuta.io>
2019-11-22 19:27:01 -0500

    Added an option to grab x amount

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import base64
 import discord
 from time import sleep
 from creds import get_token
-from queue import Queue
 import slickdeals_feed
 client = discord.Client()
 
@@ -10,54 +9,47 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
-buffer = Queue()
-
-
 channel = client.get_channel(1232423418372)
 # await channel.send('hello')
 
-
 @client.event
 async def on_message(message):
-#    message.
     print(message.author, " : ",  message.content)
-
     mention_user = "<@"+str(message.author.id)+">"
-
     # print(message.member)
     if message.author == client.user:
         return
-
+    
+    user_msg = message.content.lower().strip()
     # print(message.guild)
     # print(message.author.id)
     if message.content.startswith('$hello'):
         sleep(1.5)
         await message.channel.send(mention_user + " I am awaiting your orders master")
     
-    elif "!sd" ==  message.content.lower().strip() and len(message.content.split()) == 1:
+    elif "!sd" ==  user_msg.strip():
         sleep(0.5)
         hold_msgs = slickdeals_feed.get_frontpage()[0:5]
         for entry in hold_msgs:
             await message.channel.send(entry)
             sleep(0.4)
+        await message.channel.send(mention_user + " Your wish is my command master, check out these latest deals ")
 
-        await message.channel.send(mention_user + " I will serve you the slickest of deals master ")
-    elif "!sd" in message.content.lower():
-        search_term = message.content.lower().split(" ")[1:]
+
+    elif "!sd" in user_msg:
+        search_term = user_msg.split(" ")[1:]
         search_str = "+".join(search_term)
         link = f"https://slickdeals.net/newsearch.php?src=SearchBarV2&q={search_str}&searcharea=deals&searchin=first&rss=1"
-        hold_msgs = slickdeals_feed.get_item(link)[0:5]
+        if user_msg.split(" ")[-1].isnumeric():
+            grab_x = int(user_msg.split(" ")[-1])
+            hold_msgs = slickdeals_feed.get_item(link)[0:grab_x]
+        else:
+            hold_msgs = slickdeals_feed.get_item(link)[0:5]
+
         for entry in hold_msgs:
             await message.channel.send(entry)
             sleep(0.4)
-
         await message.channel.send(mention_user + " I will serve you the slickest of deals master ")
-       
-#    elif "based" in messa#ge.content.lower():
-        #sleep(1.5)
-        #await message.channel.send(mention_user + " based\n and\n redpilled\n :pill: ")
-        #sleep(1.5)
 
 def fetch_token():
     token = str(get_token())
